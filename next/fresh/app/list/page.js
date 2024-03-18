d2c2b0f0c1948d05dfd8280079380eb39f3a09b7
sonprogrammer <ods04138@gmail.com>
2024-03-18 17:40:27 +0900

    next

---
+++ next/fresh/app/list/page.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function List() {
+  return (
+    <div>
+      <h1 className='title'>hi</h1>
+      <div className="food">
+        <h4>상품1 $40</h4>
+      </div>
+      <div className="food">
+        <h4>상품2 $40</h4>
+      </div>
+    </div>
+  )
+}
