ca9db3c9236278d3702515ccd3f51da3bad4054d
sonprogrammer <ods04138@gmail.com>
2024-03-20 21:01:01 +0900

    server action

---
+++ next/forum/app/write2/page.js
@@ -0,0 +1,24 @@
+import { connectDB } from "@/util/database"
+
+export default async function Write2(){
+
+    async function handleSubmit(formData){
+        'use server'
+        const client = await connectDB
+        const db = await client.db('forum')
+        const result = await db.collection('post_test').insertOne({title: formData.get('title')})
+        console.log(result)
+        console.log('hi')
+        console.log(formData.get('title'))
+    }
+    return(
+        <div>
+            <form action={handleSubmit}>  
+            {/* console에 hi가 뜸 */}
+                <input name="title" type="text" /> 
+                {/* input의 name에 title이라고  입력한 데이터가 콘솔창에 뜸  */}
+                <button type="submit">button</button>
+            </form>
+        </div>
+    )
+}
