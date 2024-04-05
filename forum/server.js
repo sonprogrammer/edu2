6ce69b8fd8247577408358734e150f22a9c3751e
sonprogrammer <ods04138@gmail.com>
2024-04-05 19:14:36 +0900

    node

--- forum/server.js
+++ forum/server.js
@@ -1,20 +1,44 @@
 const express = require('express');
 const app = express();
 
-app.listen(8080, ()=>{
-    console.log('sever is runnding')
-})
+app.use(express.static(__dirname + '/public'))
+app.set('view engine', 'ejs');
+
+
 
 app.get('/', (req,res) =>{
-    res.send('hello')
-    // res.sendFile(__dirname + '/index.html')
+    res.send('home')
 })
 
 app.get('/news', (req,res) =>{
+    db.collection('post').insertOne({title: 'hi'})
     res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/list', async (req,res) =>{
+    let result = await db.collection('post').find().toArray()
+    console.log(result)
+    res.render('list.ejs', { posts : result})
 })
 
 app.get('/about', (req,res) =>{
     res.sendFile(__dirname + '/about.html')
 })
+app.get('/time', (req,res) =>{
+    res.render('time.ejs', { date: new Date()})
+})
 
+
+const { MongoClient } = require('mongodb')
+
+let db;
+const url = 'mongodb+srv://ods04139:cVCzld1lb8HhdUVO@cluster0.lyyr1v9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+new MongoClient(url).connect().then((client) =>{
+    console.log('db connection successful')
+    db = client.db('forum')
+    app.listen(8080, ()=>{
+        console.log('sever is runnding')
+    })
+}).catch((err) => {
+    console.error(err)
+})
