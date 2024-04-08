9a6fec77d464c3e446b1d135c23cc3954c6159e2
sonprogrammer <ods04138@gmail.com>
2024-04-08 20:02:03 +0900

    node

--- simple/server.js
+++ simple/server.js
@@ -5,3 +5,23 @@ const path = require('path')
 app.listen(8080, ()=>{
     console.log('listening on 8080')
 })
+
+app.use(express.static(path.join(__dirname, 'reactproject/build')))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'reactproject/build/index.html'))
+})
+
+
+
+
+
+
+
+
+
+
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'reactproject/build/index.html'))
+})
