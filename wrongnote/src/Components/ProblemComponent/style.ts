74ed5a8ebee46c15b3f5fa7ad3954f8d95d1e265
sonprogrammer <ods04138@gmail.com>
2024-04-23 01:04:09 +0900

    wrongnote basic setting

--- wrongnote/src/Components/ProblemComponent/style.ts
+++ wrongnote/src/Components/ProblemComponent/style.ts
@@ -5,7 +5,10 @@ export const StyledContainer = styled.div`
     ${tw`
         bg-gray-400
         rounded-xl
-        w-[200px]
+        w-[calc(40% - 10px)]
         text-wrap
     `}
+    @media(max-width:768px){
+        width: calc(100% - 10px)
+    }
 `
