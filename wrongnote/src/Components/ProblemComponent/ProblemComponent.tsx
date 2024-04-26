161a1023f2517321b62f4078011f6d5aa82f743f
sonprogrammer <ods04138@gmail.com>
2024-04-26 23:04:47 +0900

    wrongnote basic setting

--- wrongnote/src/Components/ProblemComponent/ProblemComponent.tsx
+++ wrongnote/src/Components/ProblemComponent/ProblemComponent.tsx
@@ -32,10 +32,7 @@ export default function ProblemComponent() {
         <p>description</p>
         <h1>answer</h1>
       </StyledContainer>
-      <StyledContainer>
-        <p>description</p>
-        <h1>answer</h1>
-      </StyledContainer>
+     
     </StyledBox>
   )
 }
