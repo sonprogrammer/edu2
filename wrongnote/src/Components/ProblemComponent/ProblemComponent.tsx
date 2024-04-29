d9801fd80e7c8dde661ac30c1214915c72dfb5dd
sonprogrammer <ods04138@gmail.com>
2024-04-29 18:52:52 +0900

    Progress problemcomponent

--- wrongnote/src/Components/ProblemComponent/ProblemComponent.tsx
+++ wrongnote/src/Components/ProblemComponent/ProblemComponent.tsx
@@ -1,16 +1,20 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { StyledContainer, StyledBox } from './style'
+import { useSelector, useDispatch } from 'react-redux'
+import { setShowAnwser } from '../../store'
+import axios from 'axios'
 
-interface ProblemComponentProps{
-  answer: boolean;
-}
 
 export default function ProblemComponent() {
-  const[showAnswer, setShowAnswer] = useState(false)
+  const [showAnswer, setShowAnwser] = useState(false)
+  const [showAnswer2, setShowAnwser2] = useState(false)
   // const[isExpand, setIsExpand] = useState(false)
   
   const handleClick = () => {
-    setShowAnswer(!showAnswer)
+    setShowAnwser(!showAnswer)
+  }
+  const handleClick2 = () => {
+    setShowAnwser2(!showAnswer2)
   }
 
   //*설명길이가 길면 20자 이상뒤로 부턴 ...으로 대체
@@ -33,10 +37,10 @@ export default function ProblemComponent() {
       <StyledContainer answer={showAnswer}>
         <p>내 출생연도와 내 이름과 내 나이는?</p>
         { showAnswer ? (
-          <h1 onClick={handleClick}>1997.01.26 손영진</h1>
+          <h1 onClick={handleClick2}>1997.01.26 손영진</h1>
         ):
         (
-          <h1 onClick={handleClick}>Check the answer</h1>
+          <h1 onClick={handleClick2}>Check the answer</h1>
         )
       }
       </StyledContainer>
@@ -44,3 +48,4 @@ export default function ProblemComponent() {
     </StyledBox>
   )
 }
+
