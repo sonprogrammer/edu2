c5c93b6b083981f93b368f10ad1bca3125e32aa6
sonprogrammer <ods04138@gmail.com>
2024-05-20 17:35:20 +0900

    Feat : code refactoring

--- wrongnote/client/src/main.tsx
+++ wrongnote/client/src/main.tsx
@@ -7,6 +7,7 @@ import { Provider } from 'react-redux'
 import store from './store.ts'
 
 
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
     <BrowserRouter>
