7fdc8d4522472af747cdd097e955b30b3514fe59
sonprogrammer <ods04138@gmail.com>
2024-05-28 18:04:28 +0900

    vue

--- vue/vue-pr/src/App.vue
+++ vue/vue-pr/src/App.vue
@@ -1,25 +1,37 @@
-
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-    <div>
-      <h4>XX 원룸</h4>
-      <p>{{ price1 }} 만원 </p>
+
+  <div class="menu">
+    <a v-for="작명 in menus" :key="작명">{{작명}}</a>
+  </div>
+ <div>
+    <div v-for="(a, i) in products" :key="i">
+      <img :src="getImageSrc(i)" class="room-img">
+      <h4>{{ a }}</h4>
+      <p>XX 원</p>
+      <button @click="신고수[i]++">허위매물 신고</button>
+      <span>신고수 : {{ 신고수[i] }}</span>
     </div>
-    <div>
-      <h4>XX 원룸</h4>
-      <p>{{ price2 }} 만원 </p>
-    </div>
+  </div>
 </template>
 
 <script>
-
 export default {
   name: 'App',
   data(){ //데이터 보관함
     return{
       price1 : 50,
-      price2: 70
+      price2: 70,
+      products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
+      menus: ['Home', 'Shop', 'About'],
+      신고수: [0,0,0]
+    }
+  },
+  methods : {
+    increase(){
+      this.신고수++;
+    },
+      getImageSrc(index) {
+      return require(`./assets/room${index}.jpg`);
     }
   },
   components: {
@@ -34,6 +46,20 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.menu{
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+
+}
+.menu a{
+  color: white;
+  padding: 10px;
+}
+.room-img{
+  width: 100%;
+  margin-top: 40px;
 }
 </style>
