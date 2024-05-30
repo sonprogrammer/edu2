43d1e354fe443b162415dc5ba3c4a7b7791169ba
sonprogrammer <ods04138@gmail.com>
2024-05-30 16:43:01 +0900

    vue blog

---
+++ vue/blog/src/components/List.vue
@@ -0,0 +1,30 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<!-- eslint-disable vue/no-mutating-props -->
+
+<template>
+
+<div>
+
+  <div>
+    <h4>블로그 글 제목</h4>
+    <p>블로그 글 날짜</p>
+  </div>
+  <div>
+    <h4>블로그 글 제목</h4>
+    <p>블로그 글 날짜</p>
+  </div>
+
+</div>
+
+</template>
+
+<script>
+export default {
+    name: 'List'
+
+}
+</script>
+
+<style>
+
+</style>
