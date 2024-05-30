f1c7a1a37c488367294bb35e32c27baffbd903bd
sonprogrammer <ods04138@gmail.com>
2024-05-30 18:52:43 +0900

    vue instagram

--- vue/vuestagram/src/App.vue
+++ vue/vuestagram/src/App.vue
@@ -1,27 +1,123 @@
 <template>
   <div>
-    
+    <div class="header">
+      <ul class="header-button-left">
+        <li>Cancel</li>
+      </ul>
+      <ul class="header-button-right">
+        <li>Next</li>
+      </ul>
+      <img src="./assets/logo.png" class="logo" />
+    </div>
+
+    <Container :post="post"/>
+    <button @click="more">더보기</button>
+
+    <div class="footer">
+      <ul class="footer-button-plus">
+        <input type="file" id="file" class="inputfile" />
+        <label for="file" class="input-plus">+</label>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+import Container from "./components/Container.vue";
+import Post from "./components/Post.vue";
+import data from './assets/data'
+import axios from 'axios'
 
+const result = axios.get('https://codingapple1.github.io/vue/more0.json')
 
 export default {
-  name: 'App',
+  name: "App",
+  data(){
+    return {
+      post: data,
+      plus: 0
+    }
+  },
   components: {
-
-  }
-}
+    Container: Container,
+    Post: Post,
+  },
+  methods: {
+    more(){
+      axios.get(`https://codingapple1.github.io/vue/more${this.plus}.json`)
+      .then((result) => {
+        this.post = [...this.post, result.data]
+        this.plus++
+      })
+    }
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+body {
+  margin: 0;
+}
+ul {
+  padding: 5px;
+  list-style-type: none;
+}
+.logo {
+  width: 22px;
+  margin: auto;
+  display: block;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 13px;
+}
+.header {
+  width: 100%;
+  height: 40px;
+  background-color: white;
+  padding-bottom: 8px;
+  position: sticky;
+  top: 0;
+}
+.header-button-left {
+  color: skyblue;
+  float: left;
+  width: 50px;
+  padding-left: 20px;
+  cursor: pointer;
+  margin-top: 10px;
+}
+.header-button-right {
+  color: skyblue;
+  float: right;
+  width: 50px;
+  cursor: pointer;
+  margin-top: 10px;
+}
+.footer {
+  width: 100%;
+  position: sticky;
+  bottom: 0;
+  padding-bottom: 10px;
+  background-color: white;
+}
+.footer-button-plus {
+  width: 80px;
+  margin: auto;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  cursor: pointer;
+  font-size: 24px;
+  padding-top: 12px;
+}
+.sample-box {
+  width: 100%;
+  height: 600px;
+  background-color: bisque;
+}
+.inputfile {
+  display: none;
+}
+.input-plus {
+  cursor: pointer;
 }
 </style>
---
+++ vue/vuestagram/src/components/Container.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Post :post="post[i]"  v-for="(a, i) in post" :key="i"/>
+  </div>
+</template>
+
+<script>
+import Post from "./Post.vue";
+
+export default {
+  name: "Container",
+  props: {
+    post: Array
+  },
+  components: {
+    Post: Post,
+  },
+};
+</script>
+
+<style>
+</style>
