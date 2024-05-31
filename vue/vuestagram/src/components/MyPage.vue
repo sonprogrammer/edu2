e0fa678a3ec913ce6393512c2ca714839d8e3c04
sonprogrammer <ods04138@gmail.com>
2024-05-31 18:42:27 +0900

    vuex

--- vue/vuestagram/src/components/MyPage.vue
+++ vue/vuestagram/src/components/MyPage.vue
@@ -2,7 +2,7 @@
   <div>
     <div style="padding: 10px">
       <h4>팔로워</h4>
-      <input placeholder="🔍" />
+      <input placeholder="🔍"  @input="search($event.target.value)"/>
       <div class="post-header" v-for="(a,i) in follower" :key="i"> 
         <div class="profile" :style="`background-image:url(${a.image})`"></div>
         <span class="profile-name">{{ a.name }}</span>
@@ -21,6 +21,12 @@ export default {
   setup() {
     //컴포넌트 만들기전에 setup부터 실행시킴
     let follower = ref([]); //데이터는 ref()에 담는다
+    function search(검색어){
+        let newFollower = follower.value.filter((a) => {
+            return a.name.indexOf(검색어) != -1
+        })
+        follower.value = [...newFollower]
+    }
 
     onMounted(() => {
       //장착되고나서 실행할 코드
@@ -29,7 +35,7 @@ export default {
       });
     });
 
-    return { follower }; //이렇게 해줘야 위에서  사용가능하다
+    return { follower,search }; //이렇게 해줘야 위에서  사용가능하다
   },
   data() {
     return {};
