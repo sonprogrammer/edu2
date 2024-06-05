0b71c4c1b63478be920e23ee4eedf355fca9f759
sonprogrammer <ods04138@gmail.com>
2024-06-05 16:54:18 +0900

    codingtest

--- codingtest/code3.js
+++ codingtest/code3.js
@@ -5,14 +5,14 @@
 //     } else if (date1[0] > date2[0]) {
 //         return 0;
 //     }
-    
+
 //     // 연도가 같은 경우 월 비교
 //     if (date1[1] < date2[1]) {
 //         return 1;
 //     } else if (date1[1] > date2[1]) {
 //         return 0;
 //     }
-    
+
 //     // 연도와 월이 같은 경우 날짜 비교
 //     if (date1[2] < date2[2]) {
 //         return 1;
@@ -98,21 +98,109 @@
 //     return [...str1].map((x, idx) => x+str2[idx]).join('')
 // }
 
-function solution(myString, pat){
-    let maxLength = 0
-    let sub = ''
+// function solution(myString, pat){
+//     let maxLength = 0
+//     let sub = ''
 
-    for(let i = 0; i < myString.length; i++){
-        for(let j = 0; j <= myString.length; j++){
-            let substring = myString.substring(i, j)
-            if(substring.endsWith(pat) && substring.length > maxLength){
-                maxLength = substring.length
-                sub = substring
-            }
-        }
+//     for(let i = 0; i < myString.length; i++){
+//         for(let j = 0; j <= myString.length; j++){
+//             let substring = myString.substring(i, j)
+//             if(substring.endsWith(pat) && substring.length > maxLength){
+//                 maxLength = substring.length
+//                 sub = substring
+//             }
+//         }
+//     }
+//     return sub
+// }
+
+// const solution = (str, pat) => str.substring(0, str.lastIndexOf(pat)) + pat
+
+// function solution(arr){
+//     let count = 0
+//     for(let i = 0; i < arr.length; i++){
+//         while(arr[i] > 1){
+//             if(arr[i] % 2 == 0){
+//                 arr[i] /= 2
+//             }else{
+//                 arr[i] = (arr[i]-1)/2
+//             }
+//             count++
+//         }
+//     }
+//     return count
+// }
+
+
+// function solution(arr){
+//     return arr.map(v => v.toString(2).length - 1).reduce((a, c) => a + c)
+// }
+
+// function solution(x1, x2, x3, x4){
+//     let a = false
+//     let b = false
+//     let result = false
+
+//     if(x1 === x2 === true){
+//         a = true
+//     }else{
+//         a = false
+//     }
+//      if(x3 === x4 === true){
+//         b = true
+//     }else{
+//         b = false
+//     }
+//     if(a == b == true){
+//         return true
+//     }
+
+// }
+
+
+// function solution(myStr) {
+//     let result = []
+//     let current = ''
+
+//     for (let char of myStr) {
+//         if (char === "a" || char === "b" || char === "c") {
+//             if (current !== "") {
+//                 result.push(current);
+//                 current = ""; // current 초기화
+//             }
+//         } else {
+//             current += char; // 현재 문자열에 문자 추가
+//         }
+//     }
+//     if (current !== "") {
+//         result.push(current);
+//     }
+//     if (result.length === 0) {
+//         return ["EMPTY"];
+//     }
+//         return result
+//     }
+
+
+// function solution(myStr){
+//     const tmp1 = myStr.split('a').join('b')
+//     const tmp2 = tmp1.split('b').join('c')
+//     const tmp3 = tmp2.split('c').filter(x => x)
+//     if(tmp3.length === 0) return ["EMPTY"];
+//     return tmp3
+// }
+
+function solution(myString, pat){
+    let count = 0
+    let index = myString.indexOf(pat)
+
+    while(index !== -1){
+        count++
+        index = myString.indexOf(pat, index + 1)
     }
-    return sub
+    return count
 }
 
 
-console.log(solution("AbCdEFG", "dE"))
+
+console.log(solution("banana", "ana"))
