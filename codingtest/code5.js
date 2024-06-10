9a6fe0f3f1ecae55f3aba8ade7ae80a6043c5bf1
sonprogrammer <ods04138@gmail.com>
2024-06-10 15:49:21 +0900

    codingtest

---
+++ codingtest/code5.js
@@ -0,0 +1,37 @@
+// function solution(a, b){
+//     return a - b
+// }
+
+// function solution(a, b){
+//     return Math.floor(a / b)
+// }
+
+// function solution(a, b){
+//     return a === b ? 1 : -1;
+// }
+
+// function solution(a, b){
+//     return a % b
+// }
+
+// function solution(age){
+//     return new Date().getFullYear() - age + 1
+// }
+
+// function solution(a, b){
+//     return a + b
+// }
+
+// function solution(numbers){
+//     return numbers.reduce((a, b) => a + b)/numbers.length
+// }
+
+function solution(angle){
+    if(0 < angle && angle > 90){
+        return 1
+    }else if(angle = 90){
+        
+    }
+}
+
+console.log(solution())
