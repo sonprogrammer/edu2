3a975c0b8e4f9c3a9412ae77b93e021581a49d03
sonprogrammer <ods04138@gmail.com>
2024-06-11 15:39:46 +0900

    codingtest

--- codingtest/code5.js
+++ codingtest/code5.js
@@ -128,18 +128,79 @@
 // }
 
 
-function solution(rsp){
-    let result = ''
-    for(let i = 0; i < rsp.length; i++) {
-        if(rsp[i] === '2'){
-            result += '0'
-        }else if(rsp[i] === '0'){
-            result += '5'
-        }else if(rsp[i] === '5'){
-            result += '2'
-        }
-    }   
-    return result
+// function solution(rsp){
+//     let result = ''
+//     for(let i = 0; i < rsp.length; i++) {
+//         if(rsp[i] === '2'){
+//             result += '0'
+//         }else if(rsp[i] === '0'){
+//             result += '5'
+//         }else if(rsp[i] === '5'){
+//             result += '2'
+//         }
+//     }   
+//     return result
+// }
+
+// function solution(my_string){
+//     let answer = 0;
+//     for(let i = 0; i < my_string.length; i++){
+//         if(my_string[i] >= '0' && my_string[i] <= '9'){
+//             answer += Number(my_string[i])
+//         }
+//     }
+//     return answer
+// }
+
+// function solution(m){
+//     return m.length*2
+// }
+
+// function solution(cipher, code){
+//     let result = ''
+//     for(let i = 0; i < cipher.length; i++){
+//         if((i+1) % code == 0){
+//             result += cipher[i]
+//         }
+//     }
+//     return result
+// }
+
+// function solution(my_string){
+//     let result = ''
+//     for(let i = 0; i < my_string.length; i++) {
+//         if(my_string[i] >= 'a' && my_string[i] <= 'z'){
+//             result += my_string[i].toUpperCase()
+//         }else{
+//             result += my_string[i].toLowerCase()
+//         }
+//     }
+//     return result
+// }
+
+// function solution(arr){
+//     const max = Math.max(...arr)
+//     const index = arr.indexOf(max)
+//     return [max, index]
+// }
+
+// function solution(n, numList){
+//     let result = [];
+//     for(let i = 0; i < numList.length; i++){
+//         if(numList[i] % n == 0){
+//             result.push(numList[i])
+//         }
+//     }
+//     return result
+// }
+
+// function solution(n, numList){
+//     return numList.filter(num => num % n == 0)
+// }
+
+function solution(n){
+    return n.map(a => a*2)
 }
 
-console.log(solution("205"))
+
+console.log(solution([1, 2, 3, 4, 5]))
