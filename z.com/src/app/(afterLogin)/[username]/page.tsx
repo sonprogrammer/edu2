5788ff12861717ab561f29790ca4a42ed21d6acb
sonprogrammer <ods04138@gmail.com>
2024-06-14 18:21:27 +0900

    x clone coding

---
+++ z.com/src/app/(afterLogin)/[username]/page.tsx
@@ -0,0 +1,7 @@
+export default function Profile() {
+    return (
+      <main>
+        프로필
+      </main>
+    )
+  }
---
+++ z.com/src/app/(afterLogin)/layout.tsx
@@ -0,0 +1,8 @@
+export default function AfterLoginLayout({children}) {
+    return(
+        <div>
+            afterlogin
+            {children}
+        </div>
+    )
+}
