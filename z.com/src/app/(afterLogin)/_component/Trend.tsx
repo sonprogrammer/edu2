6be2b4c87c66a6f31a75c15b1e47e0ddfb9b14ac
sonprogrammer <ods04138@gmail.com>
2024-06-25 18:35:03 +0900

    x clone coding

--- z.com/src/app/(afterLogin)/_component/Trend.tsx
+++ z.com/src/app/(afterLogin)/_component/Trend.tsx
@@ -4,7 +4,7 @@ export default function Trend() {
   return (
     <Link href={`/search?q=트렌d드`} className={style.container}>
       <div className={style.count}>실시간트렌드</div>
-      <div className={style.title}>제로초</div>
+      <div className={style.title}>youngjinSon</div>
       <div className={style.count}>1,234 posts</div>
     </Link>
   )
