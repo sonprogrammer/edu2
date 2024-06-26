17a02b27c6d20f43f86b93673ebbef1176d084a6
sonprogrammer <ods04138@gmail.com>
2024-06-26 17:13:08 +0900

    x clone coding

--- z.com/src/app/(afterLogin)/home/page.tsx
+++ z.com/src/app/(afterLogin)/home/page.tsx
@@ -1,7 +1,18 @@
+import Tab from '@/app/(afterLogin)/home/_component/Tab'
+import style from './home.module.css'
+import TabProvider from '@/app/(afterLogin)/home/_component/TabProvider'
+import PostForm from '@/app/(afterLogin)/home/_component/PostForm'
+import Post from '@/app/(afterLogin)/_component/Post'
+
 export default function Home() {
-    return (
-      <main>
-        홈페이지
-      </main>
-    )
-  }
+  return (
+    <main className={style.main}>
+      <TabProvider>
+        <Tab />
+        <PostForm />
+        <Post />
+
+      </TabProvider>
+    </main>
+  )
+}
