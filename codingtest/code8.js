810536abfa643c97b051f47619757bb182255e7c
sonprogrammer <ods04138@gmail.com>
2024-07-09 18:32:40 +0900

    codingtest

--- codingtest/code8.js
+++ codingtest/code8.js
@@ -190,6 +190,32 @@
 // }
 
 
+// function solution(arr){
+//     let answer = [];
+//     for(let i = 0; i < arr.length; i++){
+//         if(arr[i] !== arr[i-1]){
+//             answer.push(arr[i]);
+//         }
+//     }
+//     return answer;
+// }
 
+// function solution(arr){
+//     return arr.filter((val, i) => val != arr[i+1])
+// }
 
-console.log(solution(3, 12))
+function solution(d, budget){
+    d.sort((a, b) => a - b)
+    let count = 0
+    for(let i = 0; i < d.length; i++){
+        if(budget >= d[i]){
+            budget -= d[i]
+            count++
+        }else{
+            break
+        }
+    }
+    return count
+}
+
+console.log(solution([1,3,2,5,4], 9))
