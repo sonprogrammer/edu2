3f69444e5cdce24c1098c19ceb07b45ee926c439
sonprogrammer <ods04138@gmail.com>
2024-07-13 18:52:16 +0900

    codingtest

--- codingtest/code8.js
+++ codingtest/code8.js
@@ -254,6 +254,20 @@
 //     }).join(' ')
 // }
 
+function solution(sizes) {
+    let maxWidth = 0;
+    let maxHeight = 0;
+
+    sizes.forEach(([width, height]) => {
+        if (width < height) {
+            [width, height] = [height, width];
+        }
+        maxWidth = Math.max(maxWidth, width);
+        maxHeight = Math.max(maxHeight, height);
+    });
+
+    return maxWidth * maxHeight;
+}
 
 
 
