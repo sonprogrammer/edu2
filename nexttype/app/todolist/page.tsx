568d97c28c9882d656437e140e42a9e83ad43fca
sonprogrammer <ods04138@gmail.com>
2024-11-20 17:28:46 +0900

    done

---
+++ nexttype/app/todolist/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='h-full flex justify-center items-center'>
+      <div>
+        <h1>todoList</h1>
+        <input placeholder='write...' />
+        <button>+</button>
+      </div>
+      <div>
+        input
+      </div>
+    </div>
+  )
+}
+
+export default page
