6a30a39905e38a909629636c71aa999cab2252c0
sonprogrammer <ods04138@gmail.com>
2024-11-28 17:22:09 +0900

    done

--- store/src/app/map/KakaoMap.tsx
+++ store/src/app/map/KakaoMap.tsx
@@ -2,56 +2,74 @@
 
 import React, { useEffect, useRef } from 'react';
 
-const KakaoMap: React.FC = () => {
+interface KakaoMapProps {
+  onLoadComplete: () => void;
+}
+
+const KakaoMap: React.FC<KakaoMapProps> = ({ onLoadComplete }) => {
   const mapRef = useRef<HTMLDivElement | null>(null);
-  
 
   useEffect(() => {
     if (typeof window !== 'undefined') {
       const script = document.createElement('script');
       script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_APP_KEY}&autoload=false`;
       script.async = true;
-      
+  
       script.onload = () => {
-        console.log('window', window.kakao)
         if (window.kakao && window.kakao.maps) {
-          // Kakao Maps API 로드 완료 후 지도 생성
           window.kakao.maps.load(() => {
-            console.log('mapref.current', mapRef.current)
-            const mapContainer = mapRef.current;
-            if (!mapContainer) return;
+            navigator.geolocation.getCurrentPosition((position) => {
+              const { latitude, longitude } = position.coords
+              console.log('geolocation', latitude, longitude)
+              
+              const mapContainer = mapRef.current;
+              if (!mapContainer) return;
+              
+              const mapOption = {
+                center: new window.kakao.maps.LatLng(latitude, longitude),
+                level: 3,
+              };
+              
+              const map = new window.kakao.maps.Map(mapContainer, mapOption);
 
-            const mapOption = {
-              center: new window.kakao.maps.LatLng(37.5665, 126.9780),
-              level: 3,
-            };
+              const markerPosition = new window.kakao.maps.LatLng(latitude, longitude);
 
-            const map = new window.kakao.maps.Map(mapContainer, mapOption);
+              const imageSrc = './click.png'
+              const imageSize = new window.kakao.maps.Size(52, 52)
+              const imageOptions = {
+                offset: new window.kakao.maps.Point(32, 64)
+              }
 
-            const markerPosition = new window.kakao.maps.LatLng(
-              37.5665,
-              126.9780
-            );
-            const marker = new window.kakao.maps.Marker({
-              position: markerPosition,
-            });
-            marker.setMap(map);
+              const markerImage = new window.kakao.maps.MarkerImage(imageSrc, imageSize, imageOptions)
+              
+
+              const marker = new window.kakao.maps.Marker({
+                position: markerPosition,
+                image: markerImage
+              })
+              marker.setMap(map)
+              
+            })
+
+            onLoadComplete(); 
           });
         } else {
           console.error('Kakao Map SDK failed to load.');
         }
       };
-
+  
       document.head.appendChild(script);
     }
-  }, []);
+  }, [onLoadComplete]);
+  
+
   return (
     <div
       ref={mapRef}
       style={{
         width: '80%',
         height: '80%',
-        borderRadius: '50px'
+        borderRadius: '50px',
       }}
     ></div>
   );
--- store/src/app/map/page.tsx
+++ store/src/app/map/page.tsx
@@ -1,21 +1,34 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import KakaoMap from './KakaoMap';
 
 const Page: React.FC = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
   return (
     <div
       style={{
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        height: '100%', 
+        height: '100%',
         padding: '16px',
         zIndex: 10,
+        backgroundColor: 'white'
       }}
     >
-      <KakaoMap />
+      <KakaoMap onLoadComplete={() => setIsLoading(false)} />
+  {isLoading && (
+    <img
+      src="./loading.gif"
+      alt="Loading..."
+      style={{
+        position: 'absolute',
+        zIndex: 100,
+      }}
+    />
+  )}
     </div>
   );
 };
