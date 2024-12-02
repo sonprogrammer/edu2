66b067e9bc87948ff42acbff8ec5b4bac422d664
sonprogrammer <ods04138@gmail.com>
2024-12-02 16:21:06 +0900

    done

---
+++ store/src/app/Modal.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+interface ModalProps {
+  isOpen: boolean
+  closeModal: () => void
+  selectedItem: string
+}
+
+const Modal = ({ isOpen, closeModal, selectedItem }: ModalProps) => {
+  if (!isOpen) return null
+
+  return (
+    <div className='absolute inset-0 text-black flex items-center justify-center bg-black bg-opacity-50'
+      onClick={closeModal} 
+    >
+      <div className='modal bg-white w-[90%] p-6 rounded-lg relative'
+        onClick={e => e.stopPropagation()}
+      >
+          <h2 className='text-center mb-6'>{selectedItem} detail</h2>
+          <button className='absolute right-6 top-6' onClick={closeModal}>X</button>
+        <div className='modalInput flex flex-col'>
+          <input type="number" placeholder='weight' /> 
+          <input type="number" placeholder='amount' />
+          <input type="type" placeholder='name' />
+          <button className='p-3'>submit</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Modal
--- store/src/app/page.tsx
+++ store/src/app/page.tsx
@@ -1,26 +1,51 @@
+'use client'
+
+import Modal from "@/app/Modal";
 import Image from "next/image";
+import { MouseEventHandler, ReactEventHandler, useState } from "react";
 
 export default function Home() {
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false)
+  const [selectedItem, setSelectedItem] = useState<string>('')
+
+  const OpenModal =(item: string)=> {
+    setSelectedItem(item)
+    setIsModalOpen(true)
+  }
+
+  const closeModal = () => {
+    setIsModalOpen(false)
+  }
+  
   return (
-    <div className="h-full">
+    <div className="h-full w-full">
         <div className="clothes">
-          <div className="shoes hover:scale-[1.05] hover:shadow-lg transition-shadow">
+          <div 
+            className="shoes hover:scale-[1.05] hover:shadow-lg transition-shadow"
+             onClick={() => OpenModal('shoes')}
+            >
               <p className="text-white ">shoes</p>
           </div>
-          <div className="jacket hover:scale-[1.05] hover:shadow-lg transition-shadow">
+          <div className="jacket hover:scale-[1.05] hover:shadow-lg transition-shadow"
+            onClick={() => OpenModal('jacket')}
+          >
 
               <p className="text-white">jacket</p>
           </div>
-          <div className="pants hover:scale-[1.05] hover:shadow-lg transition-shadow">
+          <div className="pants hover:scale-[1.05] hover:shadow-lg transition-shadow"
+            onClick={() => OpenModal('pants')}
+          >
               <p className="text-white">pants</p>
 
           </div>
-          <div className="bedding hover:scale-[1.05] hover:shadow-lg transition-shadow">
+          <div className="bedding hover:scale-[1.05] hover:shadow-lg transition-shadow"
+            onClick={() => OpenModal('bedding')}
+          >
               <p className="text-white">bedding</p>
-
           </div>
         </div>
+        <Modal closeModal={closeModal} isOpen={isModalOpen} selectedItem={selectedItem}/>
     </div>
-    
+
   );
 } 
