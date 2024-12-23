95ea67180d3895f53385dbf5b2a91af7bacc5a93
sonprogrammer <ods04138@gmail.com>
2024-12-23 20:59:39 +0900

    vue

--- vuee/a/src/App.vue
+++ vuee/a/src/App.vue
@@ -1,30 +1,51 @@
 <template>
   <div>
 
+    <!-- modal -->
+    <div class="black-bg" v-if="modalOpend == true" @click="modalOpend = false">
+      <div class="white-bg" @click.stop>
+        <h4>{{rooms[clicked].title}}</h4>
+        <p>{{rooms[clicked].content}}</p>
+        <p class="close" @click="modalOpend = close">X</p>
+      </div>
+    </div>
+
     <div class="menu">
       <a v-for="a in menus" :key="a" :href="a">{{ a }}</a>
     </div>
-  <div v-for="a in products" :key="a">
-    <h4>{{a}}</h4>
-    <p>{{price2}}</p>
-    <button @click="reports++">허위매물신고</button>
-    <span>신고수 : {{ reports }}</span>
+
+    
+  <div v-for="(a,i) in rooms" :key="a">
+    <img :src="`https://codingapple1.github.io/vue/room${i}.jpg`" alt="image" class="room-img" @click="modalOpendF(i)">
+    <h4 >{{a.title}}</h4>
+    <p>{{a.price}} Won</p>
+    <button @click="increase(i)">허위매물신고</button>
+    <span>신고수 : {{ a.report }}</span>
   </div>
 
   </div>
 </template>
 
 <script>
+import data from './assets/room'
 
 export default {
   name: 'App',
   data(){
     return{
-      price1 : 10,
-      price2 : 20,
-      products : ['역삼동 원룸', '천호동 원룸', '마포구 원룸'],
       menus: ['Home', 'Shop', 'About'],
-      reports: 0
+      modalOpend: false,
+      rooms: data,
+      clicked: 0
+    }
+  },
+  methods: {
+    increase(i){
+      this.rooms[i].report++;
+    },
+    modalOpendF(i){
+      this.modalOpend = true;
+      this.clicked = i;
     }
   },
   components: {
@@ -54,4 +75,53 @@ export default {
   padding: 10px;
   text-decoration: none;
 }
+
+.room-img{
+  width: 80%;
+  margin-top: 40px;
+}
+
+body{
+  margin: 0;
+}
+div{
+  box-sizing: border-box;
+}
+
+.black-bg{
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+}
+
+.white-bg{
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+  position: relative;
+}
+
+.close{
+  position: absolute;
+  right: 10px;
+  top: 10px;
+  cursor: pointer;
+  width: 20px; 
+  height: 20px; 
+  padding: 5px; 
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  transform: translate(-10px, -10px);
+}
+.close:hover{
+  background-color: black;
+  border-radius: 50%;
+  color: white;
+  }
+
 </style>
