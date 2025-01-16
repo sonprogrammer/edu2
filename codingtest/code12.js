20e9b54d2b661f26f64b2b83f0b71f2d19ad5d37
sonprogrammer <ods04138@gmail.com>
2025-01-16 21:23:43 +0900

    codingtest

--- codingtest/code12.js
+++ codingtest/code12.js
@@ -98,18 +98,146 @@
 // }
 
 //최대공약수 하는 함수
-function gcd(){
+// function gcd(a, b){
+//     while(b !== 0){
+//         let temp = b;
+//         b = a % b
+//         a = temp
+//     }
+//     return a
+// }
 
-}
+// // 최소공배수 구하는 함수
+// function lcm(a, b) {
+//     return a * b /gcd(a, b)
+// }
 
-// 최소공배수 구하는 함수
-function lcm(a, b) {
-    return a * b /gcd(a, b)
-}
+// function solution(arr){
+//     return arr.reduce((acc, curr) => lcm(acc, curr), 1 )
+// }
 
-function solution(arr){
+
+//가장 먼저 탈락하는 사람의 번호와 그 사람이 자신의 몇 번째 차례에 탈락하는지를 구해서 return
+// function solution(n, word){
+//     let result = [] 
+//     for(let i = 0; i < word.length; i += n){
+//         result.push(word.slice(i, i + n))
+//     }
+//     if()
+
+//     return result
+// }
+
+// function solution(n, word){
+//     const usedWords = new Set()
+//     usedWords.add(word[0])
+
+//     for(let i = 1; i < word.length; i++){
+//         const currentWord = word[i]
+//         const previousWord = word[i - 1]
+
+//         if(
+//             usedWords.has(currentWord) ||
+//             previousWord[previousWord.length - 1] !== currentWord[0]
+//         ){
+//             const person = (i % n) + 1
+//             const turn = Math.floor(i / n) + 1
+//             return [person, turn]
+//         }
+//         usedWords.add(currentWord)
+//     }
+//     return [0 ,0 ]
+// }
+
+
+// function solution(n, word){
+//     let firstWord = new Set()
+//     firstWord.add(word[0])
+
+//     for(let i = 1; i < word.length; i ++){
+//         const currentWord = word[i]
+//         const previousWord = word[i - 1]
+
+//         //같은단어가 나오거나 끝말잇기가 아닐시
+//         if(
+//             firstWord.has(word[i]) ||
+//             previousWord[previousWord.length - 1] !== currentWord[0]
+//         ){
+//             const person = (i % n) + 1 //몇번째 사람인지
+//             const turn = Math.floor(i / n) + 1 //그 사람이 몇번째 턴인지
+//             console.log(firstWord)
+//             return [person, turn]
+//         }
+        
+//         firstWord.add(currentWord)
+//     }
+//     console.log(firstWord)
+//     return [0, 0]
+// }
+
+
+// console.log(solution(3, ["tank", "kick", "know", "wheel", "oand", "dream"]));
+
+// function solution(k, tan){
+//     let count = new Set()
+
+//     const sizeCount = {}
+//     tan.forEach(size => {
+//         sizeCount[size] = (sizeCount[size] || 0) + 1
+//     })
+
+//     const counts = Object.values(sizeCount).sort((a, b) => b - a)
+//     console.log(counts)
+
+//     let selectedCount = 0
+//     let types = 0
+
+//     for(let count of counts){
+//         selectedCount += count
+//         types++
+
+//         if(selectedCount >= k){
+//             break;
+//         }
+//     }
+//     return types
+
+// }
+
+// function solution(k, tan){
+//     let sizeCount = {}
+//     tan.forEach(size => {
+//         sizeCount[size] = (sizeCount[size] || 0) + 1
+//     })
+
+//     let counts = Object.values(sizeCount).sort((a, b) => b - a)
+
+//     let selectedCount = 0
+//     let key = 0
+
+//     for(let count of counts){
+//         selectedCount += count
+//         key++
+//         if(selectedCount >= k){
+//             break;
+//         }
+//     }
+
     
+//     return key
+    
+// }
 
+function solution(n, a, b){
+    let round = 0
+
+    while(a !== b){
+        a = Math.ceil(a /2 )
+        b = Math.ceil(b / 2)
+        round++
+    }
+    return round
 }
 
-console.log(solution([1,2,3]));
+
+console.log(solution(8, 4, 7))
